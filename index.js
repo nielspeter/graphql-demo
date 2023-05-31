f63f20c9cd62d6f992de23d1b8ac98a9cebed26b
nielspeter <nielspeter@npstrandberg.com>
2023-05-31 21:00:00 +0200

    2 files modified

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-var express = require('express');
-var { graphqlHTTP } = require('express-graphql');
-var { buildSchema } = require('graphql');
+import { buildSchema, graphql } from 'graphql';
 
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
@@ -9,21 +7,26 @@ var schema = buildSchema(`
   }
 `);
 
-// The root provides a resolver function for each API endpoint
-var root = {
-  hello: () => {
-    return 'Hello, world!';
-  }
+/**
+ * The root provides a resolver functions
+ *
+ * @param {any} parent - The result of the parent resolver function.
+ * @param {Object} args - An object containing the arguments passed to the query field.
+ * @param {Object} context - An object containing shared data or functionality for all resolvers.
+ * @param {Object} info - An object containing information about the execution state of the query.
+ */
+var rootValue = {
+  hello: (parent, args, context, info) => {
+    return 'Hello, world!!';
+  },
 };
 
-var app = express();
-app.use(
-  '/',
-  graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true
-  })
-);
-app.listen(4000);
-console.log('Running a GraphQL API server at http://localhost:4000/');
+var query = '{ hello }';
+
+graphql({
+  schema,
+  source: query,
+  rootValue,
+}).then((response) => {
+  console.log(JSON.stringify(response, null, 2));
+});
